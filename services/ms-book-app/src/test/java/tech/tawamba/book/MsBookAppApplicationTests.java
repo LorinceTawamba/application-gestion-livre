2e926b509fa486429c10ae99bfe6237c6f77d3d0
Lorince Tawamba <lorince.tawamba@gmail.com>
2025-02-02 22:38:42 -0500

    Création du microservice et configuration de base

---
+++ services/ms-book-app/src/test/java/tech/tawamba/book/MsBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tawamba.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
