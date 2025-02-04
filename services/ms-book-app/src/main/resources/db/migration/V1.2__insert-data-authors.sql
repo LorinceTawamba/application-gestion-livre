a100d2f0110599407f80cf9da146906d227a2718
Lorince Tawamba <lorince.tawamba@gmail.com>
2025-02-04 17:29:45 -0500

    Mis a jour des donnees

--- services/ms-book-app/src/main/resources/db/migration/V1.2__insert-data-authors.sql
+++ services/ms-book-app/src/main/resources/db/migration/V1.2__insert-data-authors.sql
@@ -1,7 +1,7 @@
 INSERT INTO authors (first_name, last_name, email, phone_number, birth_date)
 VALUES
-    ('Lorince', 'TAWAMBA', 'lorince.tawamba@gmailcom', '+1 (450) 881-8524', '2016-06-22 19:10:25-07'),
-    ('Charlotte', 'TIGOMO', 'charlotte.tigomo@gmailcom', '+1 (225) 523-9933', '1979-07-18 10:10:25-07'),
-    ('Alice', 'MATAM', 'alice.matam@gmailcom', '+237 696 789 881', '1999-11-02 23:10:25-07'),
-    ('Sara', 'FOMO', 'sara.fomo@gmailcom', '+33 (498) 802-6524', '2019-04-07 19:10:25-07'),
-    ('Julie', 'NGUIMFACK', 'julie.nguimfack@gmailcom', '+44 (325) 222-1122', '2014-07-07 07:10:32-07');
+    ('Lorince', 'TAWAMBA', 'lorince.tawamba@gmailcom', '+1 (354) 233-3310', '2016-06-22 19:10:25-07'),
+    ('Socrate', 'KOUAKAM', 'socrate.kouakam@gmailcom', '+1 (225) 523-9933', '1979-07-18 10:10:25-07'),
+    ('Alain', 'FOKA', 'alain.foka@gmailcom', '+237 696 789 881', '1999-11-02 23:10:25-07'),
+    ('Jean', 'MEDOUGA', 'jean.medouga@gmailcom', '+33 (498) 802-6524', '2019-04-07 19:10:25-07'),
+    ('Vicky', 'LARSONE', 'vicky.larsone@gmailcom', '+44 (325) 222-1122', '2014-07-07 07:10:32-07');
